af2da3349b171540e8a3a5fbb421c906c976e4e8
Rayssa <rayssa.a.vieira@outlook.com>
2021-05-06 19:52:55 +0200

    consumindo API

--- coronavirus.js
+++ coronavirus.js
@@ -32,10 +32,53 @@ const showData = ( data ) => {
 
     const $container = document.createElement('div');
     $container.innerHTML = panel;
+
     
     const $info = document.getElementById('info');
-
+    $info.removeChild ($info.firstChild);
     $info.appendChild($container);
 };
 
+const getCoronaBrazil = async() => {
+    const url = 'https://covid19-brazil-api.now.sh/api/report/v1/brazil';
+    const getApi = await fetch (url);
+    const json = await getApi.json();
+    const brazil = await{
+                    "uf" : "Brazil",
+                    "suspeitos":json.data.cases,
+                    "confirmados":json.data.confirmed,
+                    "mortes":json.data.deaths
+    }
+    showData (brazil);
+
+}
+
+const getCoronaState = async () => {
+    const url = 'https://covid19-brazil-api.now.sh/api/report/v1/';
+    const getApi = await fetch (url);
+    const json = await getApi.json();
+    BD = await json.data;
+
+}
+const findState = (evento) =>{
+    const ufMap = evento.target.getAttribute('name');
+    const getState = BD.find(estado => estado.state == ufMap);
+    const state = {
+        "uf":getState.uf,
+        "suspeitos":getState.suspects,
+        "confirmados":getState.cases,
+        "mortes":getState.deaths
+    }
+    showData ( state );
+   
+}
+
+const objectElement = document.getElementById( 'brsvg' )
+
+objectElement.addEventListener("load",function(){
+    objectElement.contentDocument.addEventListener( 'click', findState);
+});
+
 showData( BD[0])
+getCoronaState ();
+getCoronaBrazil ();
